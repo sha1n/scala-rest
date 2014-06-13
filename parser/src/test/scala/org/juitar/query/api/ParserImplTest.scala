c394d8f31caff0fac0d8c624a62ff27fd3bbe838
sha1n <shai.nagar@gmail.com>
2014-06-14 00:42:11 +0300

    Experimenting with specs2

---
+++ parser/src/test/scala/org/juitar/query/api/ParserImplTest.scala
@@ -0,0 +1,60 @@
+package org.juitar.query.api
+
+import org.juitar.query.api.model._
+import org.specs2.mutable._
+
+/**
+ * @author sha1n
+ * @since 6/13/14
+ */
+class ParserImplTest extends Specification {
+
+  "select" should {
+    "succeed parsing basic field list" in {
+      val parser: ParserImpl = new ParserImpl
+      parser.parseSelect("a, b, c") mustEqual Select(Seq(Field("a"), Field("b"), Field("c")))
+    }
+
+    "succeed parsing multipart fields list" in {
+      val parser: ParserImpl = new ParserImpl
+      parser.parseSelect("a.d.e, b, c") mustEqual Select(Seq(Field("a.d.e"), Field("b"), Field("c")))
+    }.pendingUntilFixed("Specs2 claims not equal for some reason")
+
+    "succeed parsing number and string expression list" in {
+      val parser: ParserImpl = new ParserImpl
+      parser.parseSelect("1, 'str', c") mustEqual Select(Seq(NumberExpression("1"), StringExpression("'str'"), Field("c")))
+    }
+
+  }
+
+  "filter" should {
+    "succeed parsing basic condition" in {
+      val parser: ParserImpl = new ParserImpl
+      parser.parseFilter("a < 1") mustEqual Filter(ComparisonCondition(CompConditionOp.LT, Field("a"), NumberExpression("1")))
+    }
+
+    "succeed parsing logical condition" in {
+      val parser: ParserImpl = new ParserImpl
+      parser.parseFilter("a < 1 and a > 0") mustEqual Filter(LogicalCondition(LogicalOp.AND,
+        Seq(ComparisonCondition(CompConditionOp.LT, Field("a"), NumberExpression("1")),
+          ComparisonCondition(CompConditionOp.GT, Field("a"), NumberExpression("0")))))
+    }
+
+    "succeed parsing wrapped logical condition" in {
+      val parser: ParserImpl = new ParserImpl
+      parser.parseFilter("(a < 1 AND a > 0) OR (b = c AND d != 'kuki')") mustEqual Filter(
+        LogicalCondition(LogicalOp.OR,
+          Seq(
+            WrappedCondition(LogicalCondition(LogicalOp.AND,
+              Seq(ComparisonCondition(CompConditionOp.LT, Field("a"), NumberExpression("1")),
+                ComparisonCondition(CompConditionOp.GT, Field("a"), NumberExpression("0"))))),
+            WrappedCondition(LogicalCondition(LogicalOp.AND,
+              Seq(ComparisonCondition(CompConditionOp.EQ, Field("b"), NumberExpression("c")),
+                ComparisonCondition(CompConditionOp.NE, Field("d"), StringExpression("'kuki'")))))
+          )
+        )
+      )
+    }.pendingUntilFixed("LPAREN is not properly parsed - pending fix")
+
+  }
+}
