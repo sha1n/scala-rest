055acbc6b1b1b3c56eb16406648d8da1fba4dff2
sha1n <sha1n@users.noreply.github.com>
2014-06-14 22:09:45 +0300

    Replaced usage of "name" of enums by explicit toString implementation

--- parser/src/main/java/org/juitar/query/api/model/LogicalOp.java
+++ parser/src/main/java/org/juitar/query/api/model/LogicalOp.java
@@ -5,6 +5,18 @@ package org.juitar.query.api.model;
  * @since 6/12/14
  */
 public enum LogicalOp {
-    AND,
-    OR
+    AND("and"),
+    OR("or");
+
+    private final String str;
+
+    LogicalOp(String str) {
+        this.str = str;
+    }
+
+
+    @Override
+    public final String toString() {
+        return str;
+    }
 }
--- parser/src/main/java/org/juitar/query/api/model/OrderDirection.java
+++ parser/src/main/java/org/juitar/query/api/model/OrderDirection.java
@@ -5,6 +5,18 @@ package org.juitar.query.api.model;
  * @since 6/13/14
  */
 public enum OrderDirection {
-    ASC,
-    DESC
+    ASC("asc"),
+    DESC("desc");
+
+    private final String str;
+
+    OrderDirection(String str) {
+        this.str = str;
+
+    }
+
+    @Override
+    public final String toString() {
+        return str;
+    }
 }
