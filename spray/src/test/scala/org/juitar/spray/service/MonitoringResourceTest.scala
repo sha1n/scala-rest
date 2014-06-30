0a4f00dfa3533e902090298367002fce416d2d38
sha1n <sha1n@users.noreply.github.com>
2014-06-30 09:08:22 +0300

    Composing multiple resource definitions into one sevice

--- spray/src/test/scala/org/juitar/spray/service/MonitoringResourceTest.scala
+++ spray/src/test/scala/org/juitar/spray/service/MonitoringResourceTest.scala
@@ -1,6 +1,5 @@
 package org.juitar.spray.service
 
-import org.juitar.query.api.model.Query
 import org.specs2.mutable.SpecificationWithJUnit
 import spray.http.StatusCodes._
 import spray.testkit.Specs2RouteTest
@@ -9,37 +8,30 @@ import spray.testkit.Specs2RouteTest
  * @author shain
  * @since 6/25/14
  */
-class ResourceTest extends SpecificationWithJUnit with Specs2RouteTest with Resource {
+class MonitoringResourceTest extends SpecificationWithJUnit with Specs2RouteTest with MonitoringResource {
   def actorRefFactory = system
 
-  "RestService" should {
+  "MonitoringResource" should {
 
     "return a greeting for GET requests to the root path" in {
-      Get() ~> root ~> check {
+      Get() ~> resourcePath ~> check {
         responseAs[String] must contain("I'm alive")
       }
     }
 
 
     "return a MethodNotAllowed error for PUT requests to the root path" in {
-      Put() ~> sealRoute(root) ~> check {
+      Put() ~> sealRoute(resourcePath) ~> check {
         status === MethodNotAllowed
       }
     }
 
     // todo: why response status cannot be evaluated? Client does get the expected 404
     "leave GET requests to other paths unhandled" in {
-      Get("/404") ~> root ~> check {
+      Get("/404") ~> resourcePath ~> check {
         handled must beFalse
       }
     }
   }
 
-  "echo-query" should {
-    "echo parsed query expression" in {
-      Get("/echo-query?select=a,b.c&filter=a>b&order=a,b") ~> root ~> check {
-        responseAs[String] mustEqual Query(select = "a,b.c", filter = "a>b", order = "a,b").toString
-      }
-    }
-  }
 }
