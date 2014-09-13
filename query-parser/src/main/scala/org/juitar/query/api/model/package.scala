881dae7eb58e953d17030333fe44780c195d36c2
sha1n <shai.nagar@gmail.com>
2014-09-13 23:14:13 +0300

    small cosmetic change...

--- query-parser/src/main/scala/org/juitar/query/api/model/package.scala
+++ query-parser/src/main/scala/org/juitar/query/api/model/package.scala
@@ -17,4 +17,58 @@ package object model {
   final val OP_GREATER = ">"
   final val OP_LESS_OR_EQUAL = "<="
   final val OP_GREATER_OR_EQUAL = ">="
+
+  trait QueryElement
+
+  case class Query(select: Option[Select], filter: Option[Filter], order: Option[Order]) extends QueryElement {
+    def isEmpty = select.isEmpty && order.isEmpty && filter.isEmpty
+  }
+
+  case class Select(expr: Seq[Expression]) extends QueryElement {
+    override def toString: String = expr.mkString(", ")
+  }
+
+  case class Filter(cond: Condition) extends QueryElement {
+    override def toString: String = cond.toString
+  }
+
+  case class Order(orderExprs: Seq[OrderExpression]) extends QueryElement {
+    override def toString: String = orderExprs.mkString(", ")
+  }
+
+  abstract class Condition extends QueryElement
+
+  case class WrappedCondition(cond: Condition) extends Condition {
+    override def toString: String = s"($cond)"
+  }
+
+  case class LogicalCondition(op: LogicalOp, conds: Seq[Condition]) extends Condition {
+    override def toString: String = conds.mkString(s" ${op.toString} ")
+  }
+
+  case class ComparisonCondition(op: CompConditionOp, left: Expression, right: Expression) extends Condition {
+    override def toString: String = s"$left $op $right"
+  }
+
+  abstract class Expression extends QueryElement
+
+  case class Field(qualifiers: Seq[String]) extends Expression {
+    override def toString: String = qualifiers.mkString(".")
+  }
+
+  object Field {
+    def apply(name: String): Field = new Field(Seq(name))
+  }
+
+  case class OrderExpression(expr: Expression, dir: OrderDirection = OrderDirection.ASC) extends Expression {
+    override def toString: String = s"$expr ${dir.toString}"
+  }
+
+  case class StringExpression(s: String) extends Expression {
+    override def toString: String = s
+  }
+
+  case class NumberExpression(s: String) extends Expression {
+    override def toString: String = s
+  }
 }
